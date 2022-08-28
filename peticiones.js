590081b7e20d6a6d574aa8948ef8aecfbe3b2a29
wedjat11 <jesusalfonsomontiel@gmail.com>
2022-08-28 17:07:32 -0500

    first commit

---
+++ peticiones.js
@@ -0,0 +1,42 @@
+
+
+const key = 'RGAPI-1f6fd6c4-5279-4484-87a7-c6f4ce128d35' ;
+
+const puuidsPlayers= [];
+function dataPlayers(){
+    fetch(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/thothmon?api_key=${key}`)
+        .then((response) => response.json())
+        //.then((data) => console.log(data.id))
+        .then ((data) => puuidsPlayers.push(data.id));
+    
+        fetch(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Dante621?api_key=${key}`)
+        .then((response) => response.json())
+        .then ((data) => puuidsPlayers.push(data.id));
+
+        fetch(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Humber7o?api_key=${key}`)
+        .then((response) => response.json())
+        .then ((data) => puuidsPlayers.push(data.id));
+    
+        fetch(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/SNG%20Ruben?api_key=${key}`)
+        .then((response) => response.json())
+        .then ((data) => puuidsPlayers.push(data.id));
+    
+        fetch(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Señor%20Quesito?api_key=${key}`)
+        .then((response) => response.json())
+        .then ((data) => puuidsPlayers.push(data.id));
+    
+    
+        console.log(puuidsPlayers);
+
+}
+function namePlayers(){
+puuidsPlayers.forEach(element => {
+
+    const people = element;
+    fetch(`https://la1.api.riotgames.com/lol/league/v4/entries/by-summoner/${people}?api_key=${key}`)
+        .then((response) => response.json())
+        .then((data) => console.log(data))    
+    
+});
+};
+
